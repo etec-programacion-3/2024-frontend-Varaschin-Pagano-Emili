99e215d10f411b0a9a1c43b3431f53c5662daaca
Evaraschin <116093531+Evaraschin@users.noreply.github.com>
2024-11-19 20:19:28 -0300

    Delete src directory

--- src/routes/producto.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { productoController } from '../controllers/producto.controller';
-
-const router = Router();
-
-router.get('/productos', productoController.getAll);
-router.post('/productos', productoController.create);
-
-export default router; 
